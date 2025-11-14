47a6531dd51bfa57a86f214c71820361bd197b72
mibienpanjoe <epixprint00@gmail.com>
2025-11-14 10:04:01 +0000

    Created login form and add inface component constructor and frame size

--- src/main/java/org/exercise/LoginForm.java
+++ src/main/java/org/exercise/LoginForm.java
@@ -1,7 +1,21 @@
 package org.exercise;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 
-public class LoginForm {
+public class LoginForm extends JFrame {
+    // interface component
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
 
+    //constructor
+    public LoginForm() {
+        //frame size and title
+        setTitle("Login Form");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+
+    }
 }
