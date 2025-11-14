2f367df9b90e9660fa5cf1181a2a8b894c5adb94
mibienpanjoe <epixprint00@gmail.com>
2025-11-14 16:21:32 +0000

    Book Management form implemented +CRUD implementation

--- src/main/java/org/exercise/BookManagementForm.java
+++ src/main/java/org/exercise/BookManagementForm.java
@@ -1,6 +1,12 @@
 package org.exercise;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class BookManagementForm extends JFrame {
 
@@ -21,9 +27,140 @@ public class BookManagementForm extends JFrame {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
+        //Components creation
         titleField = new JTextField();
         authorField = new JTextField();
         yearField = new JTextField();
+            // Setting  inputs width
+        titleField.setColumns(20);
+        authorField.setColumns(20);
+        yearField.setColumns(20);
+
+        addButton = new JButton("Add Book");
+        updateButton = new JButton("Update Book");
+        deleteButton = new JButton("Delete Book");
+        viewButton = new JButton("View Books");
+
+        //Adding the created components in the Panel
+            // Here It is like we define first the input tile eith a label , and after we have the input itself
+        panel.add(new  JLabel("Title"));
+        panel.add(titleField);
+        panel.add(new  JLabel("Author"));
+        panel.add(authorField);
+        panel.add(new  JLabel("Year"));
+        panel.add(yearField);
+
+        panel.add(addButton);
+        panel.add(deleteButton);
+        panel.add(viewButton);
+
+        //Events Management logic
+        addButton.addActionListener((ActionListener)new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addBook();
+            }
+        });
+
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateBook();
+            }
+        });
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                deleteBook();
+            }
+        });
+        viewButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                viewBooks();
+            }
+        });
+        add(panel);
 
     }
+
+  // CRUD implementation
+
+    //Add books
+    private void addBook() {
+        // getting information for the inputs ad storing them in variables
+        String title = titleField.getText();
+        String author = authorField.getText();
+        int year = Integer.parseInt(yearField.getText());
+
+        try (Connection conn = DatabaseConnection.getConnection()){
+            String sql = "INSERT INTO Books (Title, Author, Year) VALUES (?, ?, ?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, title);
+            stmt.setString(2, author);
+            stmt.setInt(3, year);
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(this, "Book added successfully"); //alert
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+    //Update books
+    private  void updateBook(){
+        String title = titleField.getText();
+        String author = authorField.getText();
+        int year = Integer.parseInt(yearField.getText());
+
+        try (Connection conn = DatabaseConnection.getConnection()){
+            String sql = "UPDATE books SET author = ? , year = ? WHERE Title = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, author);
+            stmt.setInt(2, year);
+            stmt.setString(3, title);
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(this, "Book updated successfully"); //alert
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+    }}
+
+
+    //Delete books
+    private void deleteBook() {
+        String title = titleField.getText();
+
+        try (Connection conn = DatabaseConnection.getConnection()){
+            String sql = "DELETE FROM books WHERE title = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, title);
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(this, "Book deleted successfully");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // View books
+    private void viewBooks()  {
+        try(Connection conn =DatabaseConnection.getConnection()){
+            String sql = "SELECT * FROM books";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            while(rs.next()){
+                books.append("ID: ").append(rs.getInt("id"))
+                        .append(", Title: ").append(rs.getString("title"))
+                        .append(", Author: ").append(rs.getString("author"))
+                        .append(",Year: ").append(rs.getInt("year"))
+                        .append("\n");
+            }
+            JOptionPane.showMessageDialog(this, books.toString);
+
+        }catch (SQLException ex){
+            ex.printStackTrace();
+    }
+
+    }
+
 }
