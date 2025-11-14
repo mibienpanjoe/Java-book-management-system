5180200bc8ff863e0e3a8820bd7533483290733c
mibienpanjoe <epixprint00@gmail.com>
2025-11-14 14:14:04 +0000

    BookManagement class updated

--- src/main/java/org/exercise/BookManagementForm.java
+++ src/main/java/org/exercise/BookManagementForm.java
@@ -1,5 +1,29 @@
 package org.exercise;
 
-public class BookManagementForm {
+import javax.swing.*;
 
+public class BookManagementForm extends JFrame {
+
+    //Interface components
+    JTextField yearField, authorField, titleField;
+    private JButton addButton , updateButton , deleteButton, viewButton;
+
+    // constructor
+    public BookManagementForm() {
+
+        //Management frame layout
+        setTitle("Book Management Form");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        // Panel info
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        titleField = new JTextField();
+        authorField = new JTextField();
+        yearField = new JTextField();
+
+    }
 }
